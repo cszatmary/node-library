ccd6dc087a6a47bdfe9cd57be35ed1e4dc1f13bc
Christopher Szatmary <cs@christopherszatmary.com>
2019-05-26 22:08:47 -0400

    Added source files for process-utils

---
+++ packages/process-utils/src/exit.ts
@@ -0,0 +1,26 @@
+import { logError, logSuccess } from './log';
+
+/**
+ * Logs a message to stderr in red, then terminates the process.
+ * @param message The message to log.
+ * @param statusCode The status code the process should terminate with. Defaults to `1`.
+ * @param optionalParams Any additional parameters to log.
+ */
+export function exitFailure(
+  message?: any,
+  statusCode: number = 1,
+  ...optionalParams: any[]
+): never {
+  logError(message, ...optionalParams);
+  return process.exit(statusCode);
+}
+
+/**
+ * Logs a message to stdout in green, then terminates the process with status 0.
+ * @param message The message to log.
+ * @param optionalParams Any additional parameters to log.
+ */
+export function exitSuccess(message?: any, ...optionalParams: any[]): never {
+  logSuccess(message, ...optionalParams);
+  return process.exit(0);
+}
---
+++ packages/process-utils/src/log.ts
@@ -0,0 +1,19 @@
+import chalk from 'chalk';
+
+/**
+ * Logs a message to stderr in red.
+ * @param message The message to log.
+ * @param optionalParams Any additional parameters to log.
+ */
+export function logError(message?: any, ...optionalParams: any[]): void {
+  console.error(chalk.red(message, ...optionalParams));
+}
+
+/**
+ * Logs a message to stdout in green.
+ * @param message The message to log.
+ * @param optionalParams Any additional parameters to log.
+ */
+export function logSuccess(message?: any, ...optionalParams: any[]): void {
+  console.log(chalk.green(message, ...optionalParams));
+}
