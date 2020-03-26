bbd384efe0fa7c67a9f2f75a014284617fd25351
Christopher Szatmary <cs@christopherszatmary.com>
2020-03-26 18:08:32 -0400

    feat(eslint-config): Remove style.js
    Merged style.js with prettier.js and removed useless rules
    
    BREAKING CHANGE: style.js deleted

--- packages/eslint-config/prettier.js
+++ packages/eslint-config/prettier.js
@@ -1,7 +1,13 @@
 module.exports = {
-  plugins: ['prettier'],
+  extends: ["prettier"],
+  plugins: ["prettier"],
   rules: {
-    'prettier/prettier': ['error'],
+    "prettier/prettier": ["error"],
+    "no-underscore-dangle": ["off"],
+    camelcase: ["error", { properties: "always" }],
+    "lines-around-directive": ["off"],
+    "no-plusplus": ["off"],
+    "spaced-comment": ["error", "always", { markers: ["/"] }],
   },
   env: {
     node: true,
