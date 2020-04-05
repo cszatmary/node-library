5cc1563d42dd18654578358b4b3463429741122d
Christopher Szatmary <cs@christopherszatmary.com>
2020-04-05 15:59:34 -0400

    feat(eslint-config): Forbid non-null assertions

--- packages/eslint-config/typescript.js
+++ packages/eslint-config/typescript.js
@@ -23,6 +23,7 @@ module.exports = {
     "@typescript-eslint/explicit-function-return-type": ["error"],
     "@typescript-eslint/no-explicit-any": ["error"],
     "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-non-null-assertion": ["error"],
     "import/no-unresolved": ["off"],
   },
   env: {
