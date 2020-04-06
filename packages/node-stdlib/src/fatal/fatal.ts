d8cb55245f3557afa2e6c08cf02940d3b8357c9e
Christopher Szatmary <cs@christopherszatmary.com>
2020-04-05 22:04:24 -0400

    feat!(node-stdlib): Make error interface global

--- packages/node-stdlib/src/fatal/fatal.ts
+++ packages/node-stdlib/src/fatal/fatal.ts
@@ -1,8 +1,6 @@
 // Copyright (c) 2020 Christopher Szatmary <cs@christopherszatmary.com>
 // All rights reserved. MIT License.
 
-import { error } from "../errors/mod";
-
 let shouldShowErrDetail = false;
 
 export function showErrDetail(b: boolean): void {
