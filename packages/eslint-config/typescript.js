f5710076a851433ee2db8189dd8344d278c4f131
Christopher Szatmary <cs@christopherszatmary.com>
2020-08-31 11:04:37 -0400

    feat(eslint-config): Configure naming-convention and ban-ts-comment

--- packages/eslint-config/typescript.js
+++ packages/eslint-config/typescript.js
@@ -19,10 +19,12 @@ module.exports = {
   ],
   rules: {
     camelcase: ["off"],
-    "@typescript-eslint/camelcase": ["error", { properties: "always" }],
+    "no-use-before-define": ["off"],
+    "@typescript-eslint/naming-convention": ["error"],
     "@typescript-eslint/explicit-function-return-type": ["error"],
     "@typescript-eslint/no-explicit-any": ["error"],
     "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-use-before-define": ["error"],
     "@typescript-eslint/no-non-null-assertion": ["error"],
     "no-useless-constructor": ["off"],
     "@typescript-eslint/no-useless-constructor": ["error"],
@@ -30,6 +32,7 @@ module.exports = {
     "@typescript-eslint/no-dupe-class-members": ["error"],
     "import/no-unresolved": ["off"],
     "consistent-return": ["off"],
+    "@typescript-eslint/ban-ts-comment": ["error", { "ts-expect-error": "allow-with-description" }],
   },
   env: {
     node: true,
