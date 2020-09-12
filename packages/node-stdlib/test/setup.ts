ff297aa37b84bd4c09317aacc9c9c92326e7af46
Christopher Szatmary <cs@christopherszatmary.com>
2020-09-12 18:38:47 -0400

    chore: Add better failure messages to toPanic matcher

--- packages/node-stdlib/test/setup.ts
+++ packages/node-stdlib/test/setup.ts
@@ -10,12 +10,37 @@ declare global {
 expect.extend({
   // eslint-disable-next-line @typescript-eslint/ban-types
   toPanic(received: Function, expected: unknown): jest.CustomMatcherResult {
+    const {
+      matcherErrorMessage,
+      matcherHint,
+      printExpected,
+      printReceived,
+      printWithType,
+    } = this.utils;
+
+    // Make sure expected is a string if it is present
+    if (expected !== undefined && typeof expected !== "string") {
+      throw new Error(
+        matcherErrorMessage(
+          matcherHint("toPanic"),
+          `${this.utils.EXPECTED_COLOR("expected")} value must be a string`,
+          printWithType("Expected", expected, printExpected),
+        ),
+      );
+    }
+
     let thrown: Error | undefined;
 
     if (this.promise && received instanceof Error) {
       thrown = received;
     } else if (typeof received !== "function") {
-      throw new Error("toPanic: received value must be a function");
+      throw new Error(
+        matcherErrorMessage(
+          matcherHint("toPanic"),
+          `${this.utils.RECEIVED_COLOR("received")} value must be a function`,
+          printWithType("Received", received, printReceived),
+        ),
+      );
     } else {
       try {
         received();
@@ -27,24 +52,33 @@ expect.extend({
     if (!thrown || !(thrown instanceof Error) || thrown.name !== "panic") {
       return {
         pass: false,
-        message: (): string => "expected function to panic",
+        message: (): string => `${matcherHint(".toPanic")}
+
+Expected received function to panic`,
       };
     }
 
     if (expected === undefined) {
       return {
         pass: true,
-        message: (): string => "expected function to not panic",
+        message: (): string => `${matcherHint(".not.toPanic")}
+
+Expected received function to not panic`,
       };
     }
 
-    if (typeof expected !== "string") {
-      throw new Error("expected value must be a string");
-    }
+    const { message } = thrown;
+    const pass = expected === message;
 
     return {
-      pass: expected === thrown.message,
-      message: (): string => `expected function to panic with message ${expected}`,
+      pass,
+      message: (): string => `${matcherHint(".toPanic")}
+
+Expected to panic with message:${pass ? " not" : ""}
+  ${printExpected(expected)}
+
+Received
+  ${printReceived(message)}`,
     };
   },
 });
